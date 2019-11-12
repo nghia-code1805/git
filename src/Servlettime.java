1194ce34f779753e41a5c2006836524800f2096d
root <root@localhost.localdomain>
2019-11-12 23:56:07 +0700

    tim

---
+++ src/Servlettime.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "Servlettime",urlPatterns = "/")
+public class Servlettime extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer=response.getWriter();
+        writer.println("<html>");
+        Date date= new Date();
+        writer.println("<h1>"+date+"</h1>");
+        writer.println("<h2>hello word</h2>");
+        writer.println("</html");
+    }
+}
